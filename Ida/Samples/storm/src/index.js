959cc15acb239c8a1c3251d7efe4c84ec817f54a
Andriy Tevelyev <info@innerbytes.com>
2025-12-26 22:03:00 +0100

    Undo test changes to the storm sample

--- Ida/Samples/storm/src/index.js
+++ Ida/Samples/storm/src/index.js
@@ -61,11 +61,7 @@ scene.addEventListener(scene.Events.afterLoadScene, (sceneId, loadMode) => {
     const mage = scene.getObject(meteoMageId);
 
     mage.setTalkColor(text.Colors.Seafoam);
-    mage.setEntity(15); // Meteomage entity (we can look entities, bodies and animation numbers in the LBArchitect application).
-
-    const bodies = mage.getAllBodies();
-    console.log("This Entity bodies count and contents:", bodies.length, bodies);
-
+    mage.setEntity(67); // Meteomage entity (we can look entities, bodies and animation numbers in the LBArchitect application).
     mage.setBody(0);
 
     // Setting collisions with environment, with other objects, and also fallable flags - normal for an actor
