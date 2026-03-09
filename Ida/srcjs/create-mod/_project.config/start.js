a2e739827ddca176a4107413a3864bac6bfa75d8
Andriy Tevelyev <info@innerbytes.com>
2026-03-09 01:17:41 +0100

    Simplifying behavior

--- Ida/srcjs/create-mod/_project.config/start.js
+++ Ida/srcjs/create-mod/_project.config/start.js
@@ -1,30 +1,13 @@
 const path = require("path");
-const { execFile, spawn } = require("child_process");
+const { execFileSync, spawn } = require("child_process");
 
 const { getArgValue, getIdaJsPath, getIdaJsServer, getPackageName } = require("./project");
 
-function runCommand(command, args, options = {}) {
-  const child = spawn(command, args, {
+function runRemote(args) {
+  const child = spawn(process.execPath, [path.join(__dirname, "run-remote.js"), ...args], {
     cwd: process.cwd(),
     stdio: "inherit",
   });
-  let interruptHandled = false;
-
-  if (options.onSigint) {
-    process.on("SIGINT", async () => {
-      if (interruptHandled) {
-        return;
-      }
-
-      interruptHandled = true;
-
-      try {
-        await options.onSigint();
-      } catch (error) {
-        console.error(error.message);
-      }
-    });
-  }
 
   child.on("exit", (code) => {
     process.exit(code ?? 0);
@@ -36,42 +19,42 @@ function runCommand(command, args, options = {}) {
   });
 }
 
-function killLocalGameProc() {
-  return new Promise((resolve) => {
-    execFile(
+function runLocal() {
+  try {
+    execFileSync(
       "powershell",
       [
         "-ExecutionPolicy",
         "Bypass",
-        "-Command",
-        "Stop-Process -Name 'LBA2' -Force -ErrorAction SilentlyContinue",
+        "-File",
+        path.join(__dirname, "run.ps1"),
+        getPackageName(),
       ],
-      () => resolve()
+      {
+        cwd: process.cwd(),
+        stdio: "inherit",
+      }
     );
-  });
+    process.exit(0);
+  } catch (error) {
+    if (typeof error.status === "number") {
+      process.exit(error.status);
+    }
+
+    console.error(error.message);
+    process.exit(1);
+  }
 }
 
 const args = process.argv.slice(2);
-const server = getArgValue("--server", args);
+const explicitServer = getArgValue("--server", args);
 const installDir = getIdaJsPath();
 const configuredServer = getIdaJsServer();
 
-if (server) {
-  runCommand(process.execPath, [path.join(__dirname, "run-remote.js"), ...args]);
+if (explicitServer) {
+  runRemote(args);
 } else if (!installDir && configuredServer) {
-  runCommand(process.execPath, [path.join(__dirname, "run-remote.js"), "--server", configuredServer]);
+  runRemote(["--server", configuredServer]);
 } else {
-  runCommand(
-    "powershell",
-    [
-      "-ExecutionPolicy",
-      "Bypass",
-      "-File",
-      path.join(__dirname, "run.ps1"),
-      getPackageName(),
-    ],
-    {
-      onSigint: killLocalGameProc,
-    }
-  );
+  runLocal();
 }
